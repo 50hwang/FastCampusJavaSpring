e8a8518f5bbbb096a3cdf80f0d4ed0e1c9ece9dd
50hwang <fiftyhwang50@knou.ac.kr>
2022-09-05 14:42:56 +0900

    220905 ch03.sec04 매서드 재정의하기(Overriding)(2)
    - 형 변환과 메소드 오버라이딩 -> 즉, 가상 메소드에 관한 예제 코드
    - CustermerTest2에 Customer2 vc = new VIPCustomer2(); 선언 후 메소드 동작

--- src/ch03/sec04/CustomerTest2.java
+++ src/ch03/sec04/CustomerTest2.java
@@ -20,6 +20,11 @@ public class CustomerTest2 {
 		price = customerKim.calcPrice(1000);	//재정의된 메소드 테스트
 		System.out.println(customerKim.showCustomerInfo() + price + "원 결제");
 		
+		Customer2 vc = new VIPCustomer2(12345, "noname");	//업 캐스팅과 메소드 오버라이딩
+		price = vc.calcPrice(1000);	//가상 메소드 : 실제 생성된 인스턴스(VIPCustomer2)의 메소드로 호출이 되었다.	
+		System.out.println(vc.showCustomerInfo() + price + "원 결제");
+
+		
 	}
 
 }
